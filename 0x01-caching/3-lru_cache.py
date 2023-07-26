4c9103d20b0bb7c529be791a671487b874f6a570
Nina S Quansah <ninaspioquansah@gmail.com>
2023-07-26 07:46:01 +0000

    blame

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
-"""MRU Caching"""
+"""LRU Caching"""
 BaseCaching = __import__("base_caching").BaseCaching
 
 
-class MRUCache(BaseCaching):
-    """Defines a caching system that uses the MRU caching algorithm
+class LRUCache(BaseCaching):
+    """Defines a caching system that uses the LRU caching algorithm
     """
     def __init__(self):
-        """Initializes an MRUCache instance"""
+        """Initializes a LRUCache instance"""
         super().__init__()
         self.__queue = list(self.cache_data.keys())
 
     def put(self, key, item):
         """
         Adds item to the self.cache_data structure,
-        using the MRU Caching algorithm
+        using the LRU Caching algorithm
         """
         cache_count = len(self.cache_data)
-        queue_count = len(self.__queue)
         if key is None or item is None:
             return None
         if cache_count == BaseCaching.MAX_ITEMS:
-            discarded_item_key = self.__queue[queue_count - 1]  # Last item
+            discarded_item_key = self.__queue[0]
             self.__queue.remove(discarded_item_key)
             self.cache_data.pop(discarded_item_key)
             print("DISCARD: {}".format(discarded_item_key))
